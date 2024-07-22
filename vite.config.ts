cce9238ada8da00659d9c48ef8a2db5081a44e21
uzo2005 <igwecletus455@gmail.com>
2024-07-22 09:36:38 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "https://cletusigwe.com/madforth/",
+  },
   build: {
     outDir: "./docs",
     emptyOutDir: true, // also necessary
